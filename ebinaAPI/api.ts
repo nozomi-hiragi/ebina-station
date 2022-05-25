c8dabf7aa2bbf8287ffa8a319f722072c308b396
nozomi hiragi <nozomi.hiragi@gmail.com>
2022-05-26 06:20:00 +0900

    DBをORMに変更

--- ebinaAPI/api.ts
+++ ebinaAPI/api.ts
@@ -1,7 +1,7 @@
 import express from "express"
 import child_process from "child_process"
 import { authKumasan } from "../utils/auth"
-import * as apidb from '../utils/APIDB'
+import * as apidb from '../utils/database'
 
 const apiRouter = express.Router()
 let entranceProc: child_process.ChildProcess | null = null
@@ -21,12 +21,7 @@ apiRouter.get('/status', authKumasan, (req, res) => {
 })
 
 apiRouter.post('/start', authKumasan, (req, res) => {
-  apidb.getAPIs((err, rows) => {
-    if (err) {
-      console.log(err)
-      return res.sendStatus(500)
-    }
-
+  apidb.getAPIs().then((rows) => {
     if (entranceProc) {
       entranceProc.kill('SIGINT')
       entranceProc = null
@@ -43,6 +38,9 @@ apiRouter.post('/start', authKumasan, (req, res) => {
       routs: rows
     }
     entranceProc.send(testObject)
+  }).catch((err) => {
+    console.log(err)
+    res.sendStatus(500)
   })
 })
 
@@ -58,60 +56,59 @@ apiRouter.post('/stop', authKumasan, (req, res) => {
 })
 
 apiRouter.get('/apis', authKumasan, (req, res) => {
-  apidb.getAPIs((err, rows) => {
-    if (err) {
-      console.log(err)
-      return res.sendStatus(500)
-    }
+  apidb.getAPIs().then((rows) => {
     res.status(200).json(rows)
+  }).catch((err) => {
+    console.log(err)
+    res.sendStatus(500)
   })
 })
 
 apiRouter.get('/api', authKumasan, (req, res) => {
   const path = (req.query.path as string)
   if (!path) { return res.sendStatus(400) }
-  apidb.getAPI(path, (err, row) => {
-    if (err) {
-      console.log(err)
-      return res.sendStatus(500)
-    }
+  apidb.getAPI(path).then((row) => {
     if (row) {
       res.status(200).json(row)
     } else {
       res.sendStatus(400)
     }
+  }).catch((err) => {
+    console.log(err)
+    res.sendStatus(500)
   })
 })
 
 apiRouter.post('/path', authKumasan, (req, res) => {
   const { path, name, type, value } = req.body
   if (!path || !name || !type || !value) { return res.sendStatus(400) }
-  apidb.setAPI(path, name, type, value, (err) => {
-    if (err) {
-      console.log(err)
-      return res.sendStatus(400)
-    }
+  apidb.setAPI(path, name, type, value).then((_) => {
     res.sendStatus(200)
+  }).catch((err) => {
+    console.log(err)
+    res.sendStatus(400)
   })
 })
 
 apiRouter.put('/path', authKumasan, (req, res) => {
   const { path, name, type, value } = req.body
   if (!path || !name || !type || !value) { return res.sendStatus(400) }
-  apidb.updateAPI(path, name, type, value, (err) => {
-    if (err) {
-      console.log(err)
-      return res.sendStatus(400)
-    }
+  apidb.updateAPI(path, name, type, value).then((_) => {
     res.sendStatus(200)
+  }).catch((err) => {
+    console.log(err)
+    res.sendStatus(400)
   })
 })
 
 apiRouter.delete('/path', authKumasan, (req, res) => {
   const path = req.query.path as string
   if (!path) { return res.sendStatus(400) }
-  apidb.deleteAPI(path, (isOk) => {
-    res.sendStatus(isOk ? 202 : 400)
+  apidb.deleteAPI(path).then((_) => {
+    res.sendStatus(202)
+  }).catch((err) => {
+    console.log(err)
+    res.sendStatus(400)
   })
 })
 
--- ebinaAPI/user.ts
+++ ebinaAPI/user.ts
@@ -9,41 +9,51 @@ const userRouter = express.Router()
 userRouter.post('/regist', (req, res) => {
   const { id, pass, name }: { id: string, pass: string, name: string } = req.body
   if (!id || !pass || !name) { return res.sendStatus(400) }
-  database.findUserFronUserTable(id, (err, user) => {
-    if (err) {
-      console.log(err)
-      res.sendStatus(406)
-    } else if (user && user.id === id) {
+  database.findUser(id).then((user) => {
+    if (user && user.id === id) {
       console.log("already have this id")
       res.sendStatus(406)
     } else {
-      database.insertUser({ id: id, name: name, pass: bcrypt.hashSync(pass, 9), })
-      res.sendStatus(201)
+      database.createUser(id, name, bcrypt.hashSync(pass, 9)).then(() => {
+        res.sendStatus(201)
+      }).catch((err) => {
+        console.log(err)
+        res.sendStatus(400)
+      })
     }
+  }).catch((err) => {
+    console.log(err)
+    res.sendStatus(406)
   })
 })
 
 userRouter.post('/login', (req, res) => {
   const { id, pass }: { id: string, pass: string } = req.body
   if (!id || !pass) { return res.sendStatus(400) }
-  database.findPassFronUserTable(id, (err, user) => {
-    if (err || !user) {
-      console.log(err)
+  database.findUserWithPass(id).then((user) => {
+    if (!user) {
       res.sendStatus(503)
     } else if (bcrypt.compareSync(pass, user.pass ?? '')) {
-      const tokens = generateToken(res, id)
-      const payload = decodeJwtToken(tokens.token)!
-      res.status(200).json({
-        id: user.id,
-        name: user.name,
-        created_at: user.created_at,
-        updated_at: user.updated_at,
-        iat: payload.iat,
-        exp: payload.exp,
+      generateToken(res, id).then((tokens) => {
+        const payload = decodeJwtToken(tokens.token)!
+        res.status(200).json({
+          id: user.id,
+          name: user.name,
+          created_at: user.created_at,
+          updated_at: user.updated_at,
+          iat: payload.iat,
+          exp: payload.exp,
+        })
+      }).catch((err) => {
+        console.log(err)
+        res.sendStatus(400)
       })
     } else {
       res.sendStatus(400)
     }
+  }).catch((err) => {
+    console.log(err)
+    res.sendStatus(503)
   })
 })
 
@@ -51,28 +61,31 @@ userRouter.post('/logout', authKumasan, (req, res) => {
   const token: string = req.cookies.token
   if (!token) { return res.sendStatus(400) }
   const payload = decodeJwtToken(token)!
-  database.findRefreshTokenFronTokenTable(payload.id, (err, token) => {
-    if (err) {
-      console.log(err)
-      res.sendStatus(503)
-    } else {
-      deleteRefreshToken(res, payload.id, (isOk) => res.sendStatus(isOk ? 200 : 401))
-    }
+  database.findRefreshTokenFronTokenTable(payload.id).then((token) => {
+    deleteRefreshToken(res, payload.id, (isOk) => res.sendStatus(isOk ? 200 : 401))
+  }).catch((err) => {
+    console.log(err)
+    res.sendStatus(503)
   })
 })
 
 userRouter.post('/refresh', authSirokuma, (req, res) => {
   const token: string = req.cookies.token
   const payload = decodeJwtToken(token)!
-  database.findRefreshTokenFronTokenTable(payload.id, (err, tokenRow) => {
-    if (err) {
-      console.log(err)
-      res.sendStatus(503)
-    } else if (tokenRow?.refresh_token === token) {
-      res.status(200).json(generateToken(res, payload.id))
+  database.findRefreshTokenFronTokenTable(payload.id).then((tokenRow) => {
+    if (tokenRow?.refresh_token === token) {
+      generateToken(res, payload.id).then((tokens) => {
+        res.status(200).json(tokens)
+      }).catch((err) => {
+        console.log(err)
+        res.sendStatus(400)
+      })
     } else {
       res.sendStatus(401)
     }
+  }).catch((err) => {
+    console.log(err)
+    res.sendStatus(503)
   })
 })
 
