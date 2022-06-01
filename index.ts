5251ff6a1da92bf94d3c49dd245c50d68c0a2e9b
nozomi hiragi <nozomi.hiragi@gmail.com>
2022-06-02 04:01:53 +0900

    WebAuthn対応

--- index.ts
+++ index.ts
@@ -2,21 +2,16 @@ import express from "express"
 import cors from "cors"
 import ebinaRouter from "./ebinaAPI/"
 import cookieParser from "cookie-parser"
-import fs from "fs"
+import { getSettings } from "./utils/settings"
 
-const SETTING_FILE_PATH = './project/setting.json'
+const settings = getSettings()
 
-if (!fs.existsSync(SETTING_FILE_PATH)) {
-  fs.writeFileSync(SETTING_FILE_PATH, '')
-}
-const setting = JSON.parse(fs.readFileSync(SETTING_FILE_PATH, 'utf8'))
-
-const port = setting.port === 'env' ? process.env.PORT : (setting.port || 3456)
+const port = settings.getPort()
 const app = express()
 app.use(express.json())
 app.use(express.text())
 app.use(cookieParser())
-if (setting.origins) app.use(cors({ origin: setting.origins, credentials: true }))
+if (settings.origins) app.use(cors({ origin: settings.origins, credentials: true }))
 
 app.use('/ebina', ebinaRouter)
 
--- utils/members.ts
+++ utils/members.ts
@@ -1,11 +1,14 @@
 import fs from 'fs'
+import base64url from 'base64url'
 
 const MEMBERS_FILE_PATH = './project/members.json'
 
 export type User = {
-  id: string,
   name: string,
-  auth: { password?: AuthPassword },
+  auth: {
+    password?: AuthPassword,
+    webAuthn?: WebAuthn,
+  },
   flags?: Flags,
 }
 
@@ -17,6 +20,28 @@ export type AuthPassword = {
   hash: string
 }
 
+export type WebAuthn = {
+  [origins: string]: WebAuthnItem | undefined
+}
+
+export type WebAuthnAuthenticator = {
+  fmt: 'fido-u2f' | 'packed' | 'android-safetynet' | 'android-key' | 'tpm' | 'apple' | 'none',
+  counter: number, // necessary
+  aaguid: string,
+  credentialID: Buffer, // necessary
+  credentialPublicKey: Buffer, // necessary
+  credentialType: 'public-key',
+  attestationObject: Buffer,
+  userVerified: boolean,
+  credentialDeviceType: 'singleDevice' | 'multiDevice',
+  credentialBackedUp: boolean,
+  transports?: ('ble' | 'internal' | 'nfc' | 'usb' | 'cable')[],
+}
+
+export type WebAuthnItem = {
+  authenticators: { [key: string]: WebAuthnAuthenticator | undefined }
+}
+
 export type Flags = {
   admin?: boolean
 }
@@ -24,11 +49,30 @@ export type Flags = {
 let members: Members
 
 const loadMembersFromFile = () => {
-  members = JSON.parse(fs.readFileSync(MEMBERS_FILE_PATH, 'utf8')) as Members
+  members = JSON.parse(fs.readFileSync(MEMBERS_FILE_PATH, 'utf8'), (key, value) => {
+    switch (key) {
+      case 'credentialID':
+      case 'credentialPublicKey':
+      case 'attestationObject':
+        return base64url.toBuffer(value)
+      default:
+        return value
+    }
+  }) as Members
 }
 
 const saveMembersToFile = () => {
-  fs.writeFileSync(MEMBERS_FILE_PATH, JSON.stringify(members))
+  fs.writeFileSync(MEMBERS_FILE_PATH, JSON.stringify(members, (key, value) => {
+    switch (key) {
+      case 'credentialID':
+      case 'credentialPublicKey':
+      case 'attestationObject':
+        if (value.type !== 'Buffer') return value
+        return base64url.encode(value.data)
+      default:
+        return value
+    }
+  }, 2))
 }
 
 loadMembersFromFile()
@@ -36,6 +80,20 @@ loadMembersFromFile()
 export const getMembers = () => members.members
 
 export const getMember = (id: string) => members.members[id]
+
+export const setMember = (id: string, user: User) => {
+  members.members[id] = user
+  saveMembersToFile()
+}
+
+export const updateMember = (id: string, callback: (user: User) => User) => {
+  const user = members.members[id]
+  if (!user) return false
+  const newUser = callback(user)
+  members.members[id] = newUser
+  saveMembersToFile()
+  return true
+}
 
 export const addMember = (id: string, member: User) => {
   if (members.members[id]) return false
