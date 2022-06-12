44eb9ca627710fda16f3cdd8ef2e8bf2fce54e5f
nozomi hiragi <nozomi.hiragi@gmail.com>
2022-06-12 11:43:22 +0900

    メソッド設定できる

--- entrance.js
+++ entrance.js
@@ -36,23 +36,49 @@ process.on('message', (message) => {
     const port = message.port || 1234
     const app = express()
     message.routs.forEach((rout) => {
+      let fnc
       switch (rout.type) {
         case 'static':
-          app.get(rout.path, (req, res) => res.status(200).json(rout.value))
+          fnc = (req, res) => res.status(200).json(rout.value)
           break;
 
         case 'JavaScript':
           const arg = (rout.value).split('>')
-          app.get(rout.path, (req, res) => {
+          fnc = (req, res) => {
             try {
               imports[arg[0]][arg[1]](req, res)
             } catch (err) {
               logGenba.error('type: JavaScript', arg, err)
               res.sendStatus(500)
             }
-          })
+          }
           break;
         default:
+          fnc = (req, res) => { res.sendStatus(404) }
+          break;
+      }
+      switch (rout.method) {
+        default:
+        case 'get':
+          app.get(rout.path, fnc)
+          break;
+        case 'head':
+          app.head(rout.path, fnc)
+          break;
+        case 'post':
+          app.post(rout.path, fnc)
+          break;
+        case 'put':
+          app.put(rout.path, fnc)
+          break;
+        case 'delete':
+          app.delete(rout.path, fnc)
+          break;
+        case 'options':
+          app.options(rout.path, fnc)
+          break;
+        case 'patch':
+          app.patch(rout.path, fnc)
           break;
       }
     });
