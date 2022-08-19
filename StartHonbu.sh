e471d3114f3ad09a365ab5bb86a443bef8f314fd
nozomi hiragi <nozomi.hiragi@gmail.com>
2022-08-19 20:11:53 +0900

    初期化用スクリプト作成

--- StartHonbu.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-deno run --allow-run --allow-read --allow-write --allow-net honbu/main.ts
---
+++ initEbinaStation.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+cd `dirname $0`
+SCRIPT_DIR=$(pwd)
+
+if !(type deno > /dev/null 2>&1); then
+  echo "Please install deno"
+  exit 1
+fi
+
+echo "Compile EbinaStation Honbu"
+deno compile --allow-run --allow-read --allow-write --allow-net -o ./generate/ebinaStationHonbu ./honbu/main.ts
+
+if (type systemctl > /dev/null 2>&1); then
+  sed -e 's/ExecStart.*/ExecStart=\/app\/ebina-station\/generate\/ebinaStationHonbu/' ebina-station.service.base > ./generate/ebina-station.service
+  echo "link ebina-station.service to systemd"
+  sudo ln -s  ${SCRIPT_DIR}/ebina-station.service /etc/systemd/system/ebina-station.service
+fi
